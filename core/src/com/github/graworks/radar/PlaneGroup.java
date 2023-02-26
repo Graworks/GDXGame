ecb3c523a40995f18811dcc891fc3b5f6f6d9ea5
graworks <evgeny.botvinnik@gmail.com>
2023-02-26 20:26:11 +0300

    minor code clean in PlaneGroup class

--- core/src/com/github/graworks/radar/PlaneGroup.java
+++ core/src/com/github/graworks/radar/PlaneGroup.java
@@ -10,7 +10,7 @@ public class PlaneGroup {
     private final List<Plane> planes = new ArrayList<>();
 
     public PlaneGroup(int size, int totalPositions) {
-        ArrayList<Integer> randomPositions = new ArrayList<Integer>();
+        ArrayList<Integer> randomPositions = new ArrayList<>();
         while (randomPositions.size() < size) {
             int a = MathUtils.random(0, totalPositions - 1);
             if (!randomPositions.contains(a)) {
