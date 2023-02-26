c2289b66d22877b614af696b3bc410d1c3efdf1e
graworks <evgeny.botvinnik@gmail.com>
2023-02-26 20:16:33 +0300

    clean comment in Launcher

--- android/src/com/github/graworks/radar/AndroidLauncher.java
+++ android/src/com/github/graworks/radar/AndroidLauncher.java
@@ -11,13 +11,5 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new RadarGame(), config);
-
-
-		/*
-		* AndroidFragmentApplication
-		* initializeForView
-		*
-		* */
-
 	}
 }
