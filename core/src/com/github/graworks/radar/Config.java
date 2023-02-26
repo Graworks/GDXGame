91d2a72f08a72d43ec0ed0d9500963e909d6b0b1
graworks <evgeny.botvinnik@gmail.com>
2023-02-26 20:21:09 +0300

    remove old version of ACCEL_TIME constant + change long numbers formatting

--- core/src/com/github/graworks/radar/Config.java
+++ core/src/com/github/graworks/radar/Config.java
@@ -4,9 +4,8 @@ public class Config {
     public static final int CIRCLES_NUMBER = 6;
     public static final int DELTA_ANGLE = 15;
     public static final int MARGIN = 20;
-    public static final long DELTA_TIME = 1200000000l;
-    //public static final long ACCEL_TIME = 60000000l;
-    public static final long ACCEL_TIME = 0l;
-    public static final long EXPLOSION_DURATION = 300000000l;
+    public static final long DELTA_TIME = 1200000000L;
+    public static final long ACCEL_TIME = 0L;
+    public static final long EXPLOSION_DURATION = 300000000L;
     public static final int GROUP_NUMBER = 14;
 }
